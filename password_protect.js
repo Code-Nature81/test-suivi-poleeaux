1dd11a210a8eef89ca49f29bcf20a62aaf1bda22
Code-Nature81 <81328065+Code-Nature81@users.noreply.github.com>
2024-10-21 14:27:40 +0200

    Add files via upload

---
+++ password_protect.js
@@ -0,0 +1,153 @@
+function checkPassword() {
+    var password = document.getElementById("passwordInput").value;
+  
+    // Comparer le mot de passe entré avec les mots de passe appropriés pour chaque page
+    //vd,l vlfd vk, fd,kfd, v,kgf bjkgf, bl gfbfnvkj f, , bjk gfkbj vfk c vcx,vlkfndjkbndjb gjknhkdgnhlkmvgbfldnfhdnfdhvngndhg
+    //gbhgfnghfnhfnhfngbnghnb000000000000000000000000000000000000000088888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888888881111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000222222222222222222222222222222221jnyhtgvrfvdfcdcfdvgbggggggbghnj,jk00000
+    
+    if (password === //bgbgfbgfbghbhgn
+    "R1@#"//kun,u,uk,jkb,yk,ky,kj,kuy,k,jk,tyvhctrgterdgterffgtrferjbvubvuhtgrnbvntghjb
+   ) {
+      window.location.href = "Materiel-Protection/materiel-protec-Castelfadeze-R01.html";
+      return false;
+      
+    } else if (password ===//14444440000000000000000000000000000000000000000000000**0*0000000000000000000000000000000000000***000000000000000000000000000000000000
+      "R1S@#")//000000000000000000000000008888888888888888888888800000000000000000000000000000000888888888888881111111111111111111111111111111111111188888888888880111111111111
+       {
+       window.location.href = "Materiel-Protection/materiel-test-copie-R1.html";
+       return false;
+     }
+    
+    else if (password ===//14444440000000000000000000000000000000000000000000000**0*0000000000000000000000000000000000000***000000000000000000000000000000000000
+     "R2@#")//000000000000000000000000008888888888888888888888800000000000000000000000000000000888888888888881111111111111111111111111111111111111188888888888880111111111111
+      {
+      window.location.href = "Materiel-Protection/materiel-protec-Puech Lapparouquial-R02.html";
+      return false;
+    } else if (password === //0000000000000000000000000888888888888888888888888888888888888888222222222222222222281111111111888888888888888888800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000/
+    "R4@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Bissardie-R4.html";
+      return false;
+    } else if (password ===//0000000000000000000000000000000000000000000000000000000000000000008888888888888888888888888888888888888888888888888888881111111111111111111111111111111111111111111111111
+     "R5@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Salvetat-R05.html";
+      return false;
+    } else if (password === "R6@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Esquilourie-R06.html";
+      return false;
+    } else if (password === "S6@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Surpresseur-Esquilourie-R6.html";
+      return false;
+    } else if (password === "R8@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Calmon-R8.html";
+      return false;
+    }else if (password === "R9@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Lacas-R9.html";
+      return false;
+    }else if (password === "R10@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Borie-Blanche-R10.html";
+      return false;
+    }else if (password === "R11@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Redouliere-R11.html";
+      return false;
+    } else if (password === "R12@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Puech-Blanc-R12.html";
+      return false;
+    } else if (password === "R15bas@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Petitarie-Bas-R15.html";
+      return false;
+    } else if (password === "R15haut@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Petitarie-Haut-R15.html";
+      return false;
+    } else if (password === "R18@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Farguettes-R18.html";
+      return false;
+    } else if (password === "R21@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Bouyssiere-R21.html";
+      return false;
+    } else if (password === "R22@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Virac-R22.html";
+      return false;
+    } else if (password === "R23@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-St-Benoit-Guignerette-R23.html";
+      return false;
+    } else if (password === "R24@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Pasteur-R24.html";
+      return false;
+    } else if (password === "R25@#1") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Guignerette1-R25.html";
+      return false;
+    } else if (password === "R25@#2") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Guignerette2-R25.html";
+      return false;
+    } else if (password === "R25@#3") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Guignerette3-R25.html";
+      return false;
+    } else if (password === "R26@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Bellevue-R26.html";
+      return false;
+    } else if (password === "R27@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Filtres-R27.html";
+      return false;
+    } else if (password === "R28@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Puech-Raynal-R28.html";
+      return false;
+    } else if (password === "R29@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Paillassarie-R29.html";
+      return false;
+    } else if (password === "R30@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Sarclars-R30.html";
+      return false;
+    } else if (password === "R31@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Pouzounac-Carmaux-R31.html";
+      return false;
+    } else if (password === "R32@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Pouzounac-Roucarie-R32.html";
+      return false;
+    } else if (password === "R33@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Lempery-R33.html";
+      return false;
+    } else if (password === "R34@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Puech-Fau-R34.html";
+      return false;
+    } else if (password === "R38@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Le-Puech-R38.html";
+      return false;
+    } else if (password === "R39@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Les Planques-R39.html";
+      return false;
+    } else if (password === "R40@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Frayssinet-R40.html";
+      return false;
+    } else if (password === "R41@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Soulie-R41.html";
+      return false;
+    } else if (password === "R43bas@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Cazouls-Bas-R43.html";
+      return false;
+    } else if (password === "R43haut@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Cazouls-Haut-R43.html";
+      return false;
+    } else if (password === "R44@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Riviere-R44.html";
+      return false;
+    } else if (password === "R45@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-Pompage-Thuries-R45.html";
+      return false;
+    } else if (password === "R46@#") {
+      window.location.href = "Materiel-Protection/materiel-proctec-Chenaie-R46.html";
+      return false;
+    } else if (password === "R47@#") {
+      window.location.href = "Materiel-Protection/materiel-protec-St-Sernin-R47.html";
+      return false;
+    } else if (password === "Carte-interactive@81/reservoirs") {
+      window.location.href = "carte-interactive.html";
+      return false;
+  
+    } else {
+      // Mot de passe incorrect, afficher un message d'erreur
+      var errorText = document.getElementById("errorText");
+      errorText.style.display = "block";
+      return false;
+    }
+  }
+  
